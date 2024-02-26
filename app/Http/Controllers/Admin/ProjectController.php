9c38a882cf8da640373300ce00f0b986d023b6c7
alegreco7 <alexgreco2002@gmail.com>
2024-02-26 16:33:57 +0100

    deleting a project from the db

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use Illuminate\Support\Str ;
 
 class ProjectController extends Controller
 {
@@ -17,7 +18,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::all();
-        return view('admin.project.index-project', compact('projects'));
+        return view('admin.projects.index-project', compact('projects'));
     }
 
     /**
@@ -27,7 +28,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create-project');
     }
 
     /**
@@ -38,7 +39,12 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        //
+        $form_data = $request->all();
+        $new_project = new Project();
+        $new_project->fill($form_data);
+        $new_project->slug = Str::slug($new_project->name, '-');
+        $new_project->save();
+        return redirect()->route('admin.project.index');
     }
 
     /**
@@ -49,7 +55,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        return view('admin.project.show-project', compact('project'));
+        return view('admin.projects.show-project', compact('project'));
     }
 
     /**
@@ -60,7 +66,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit-project', compact('project'));
     }
 
     /**
@@ -72,7 +78,10 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $form_data = $request->all();
+        $form_data['slug'] = Str::slug($form_data['name'], '-');
+        $project->update($form_data);
+        return redirect()->route('admin.project.show', ['project'=>$project]);
     }
 
     /**
@@ -83,8 +92,7 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return redirect()->route('admin.project.index');
     }
 }
-
-

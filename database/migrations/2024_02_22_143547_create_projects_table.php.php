4e7500d402c5fca028e4def52a7b0e3bd9b7373a
alegreco7 <alexgreco2002@gmail.com>
2024-02-26 16:25:39 +0100

    option nullable in end_date

--- database/migrations/2024_02_22_143547_create_projects_table.php.php
+++ database/migrations/2024_02_22_143547_create_projects_table.php.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -18,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->date('start_date');
-            $table->date('end_date');
+            $table->date('end_date')->nullable();
             $table->string('slug');
             $table->timestamps();
         });

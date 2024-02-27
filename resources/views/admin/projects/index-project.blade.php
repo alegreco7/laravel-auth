3c1190f5db2ecbd61f21045d7595d904661da7c4
alegreco7 <alexgreco2002@gmail.com>
2024-02-27 14:07:08 +0100

    image display if the project exists

--- resources/views/admin/projects/index-project.blade.php
+++ resources/views/admin/projects/index-project.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12 p-3">
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="">
-                        <h1>POJECTS</h1>
+                        <h1>PROJECTS</h1>
                     </div>
                     <div class="">
                         <a class="btn btn-warning" href="{{ route('admin.project.create') }}">Add project</a>
@@ -15,15 +15,24 @@
             @foreach ($projects as $project)
                 <div class="col-3">
                     <div class="card shadow h-100">
-                        <img class="card-img-top p-3" src="https://cdn-icons-png.flaticon.com/512/3767/3767084.png"
-                            alt="folder">
+                        <img class="card-img-top p-3"
+                            src="{{ $project->img !== null ? asset('storage/' . $project->img) : 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' }}"
+                            alt="img project">
                         <div class="card-body">
                             <h5 class="card-title">{{ $project->name }}</h5>
                             <div class="text-secondary">Data Creazione: {{ $project->start_date }}</div>
                             <div class="text-secondary">Data Fine: {{ $project->end_date }}</div>
-                            <div class="mt-2">
+                            <div class="mt-2 d-flex gap-2">
                                 <a href="{{ route('admin.project.show', ['project' => $project]) }}"
-                                    class="btn btn-warning">Visualizza</a>
+                                    class="btn btn-sm btn-primary">Visualizza</a>
+                                <a href="{{ route('admin.project.edit', ['project' => $project]) }}"
+                                    class="btn btn-sm btn-warning">Edit</a>
+                                <form action="{{ route('admin.project.destroy', ['project' => $project]) }}" method="post"
+                                    onsubmit="return confirm('Sei sicuro di voler eliminare questo project')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger">Elimina</button>
+                                </form>
                             </div>
                         </div>
                     </div>
